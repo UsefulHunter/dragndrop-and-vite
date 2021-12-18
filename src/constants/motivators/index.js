834c03ef5748e13cb49014dc71e24aede2d0cf9d
UsefulHunter <brian.icaro.dm@gmail.com>
2021-12-18 20:07:45 -0300

    feat: added assets to project

--- src/constants/motivators/index.js
+++ src/constants/motivators/index.js
@@ -3,55 +3,65 @@ export const Motivators = [
     title: "Aceitação",
     subTitle: "As pessoas ao redor aprovam o que eu faço e como eu sou",
     classname: "aceitacao",
+    imgsrc: `../../../assets/cards/aceitacao.jpeg`,
   },
   {
     title: "Curiosidade",
     subTitle: "Tenho muitas coisas para investigar e pensar",
     classname: "curiosidade",
+    imgsrc: `../../../assets/cards/curiosidade.jpeg`,
   },
   {
     title: "Meta",
     subTitle: "Meu propósito na vida reflete no trabalho que faço",
     classname: "meta",
+    imgsrc: `../../../assets/cards/meta.jpeg`,
   },
   {
     title: "Status",
     subTitle:
       "Minha posição é boa e reconhecida pelas pessoas que trabalham comigo",
     classname: "status",
+    imgsrc: `../../../assets/cards/status.jpeg`,
   },
   {
     title: "Liberdade",
     subTitle:
       "Sou independente dos outros com meu trabalho e minhas responsabilidades",
     classname: "liberdade",
+    imgsrc: `../../../assets/cards/liberdade.jpeg`,
   },
   {
     title: "Honra",
     subTitle:
       "Sinto orgulho de meus valores pessoais refletirem na forma como trabalho",
     classname: "honra",
+    imgsrc: `../../../assets/cards/honra.jpeg`,
   },
   {
     title: "Ordem",
     subTitle: "Existem regras e políticas suficientes para um ambiente estável",
     classname: "ordem",
+    imgsrc: `../../../assets/cards/ordem.jpeg`,
   },
   {
     title: "Maestria",
     subTitle:
       "Meu trabalho desafia minha competência, mas ainda está dentro de minhas habilidades",
     classname: "maestria",
+    imgsrc: `../../../assets/cards/maestria.jpeg`,
   },
   {
     title: "Poder",
     subTitle:
       "Há espaço suficiente para eu influenciar o que acontece ao meu redor",
     classname: "poder",
+    imgsrc: `../../../assets/cards/poder.jpeg`,
   },
   {
     title: "Relação",
     subTitle: "Tenho bons contatos sociais com as pessoas no meu trabalho",
     classname: "relacao",
+    imgsrc: `../../../assets/cards/relacao.jpeg`,
   },
 ];
